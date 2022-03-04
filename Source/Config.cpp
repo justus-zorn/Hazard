5a2bd17aff40751af09f1fe31340be1cbc1bbf5b
Justus Zorn <84742176+justus-zorn@users.noreply.github.com>
2022-03-04 19:19:53 +0100

    Simplified configuration options

--- Source/Config.cpp
+++ Source/Config.cpp
@@ -31,137 +31,117 @@ void Config::Reload() {
 	port = 34344;
 
 	lua_newtable(L);
-	lua_setglobal(L, "Content");
-	lua_newtable(L);
-	lua_setglobal(L, "Window");
-	lua_newtable(L);
-	lua_setglobal(L, "Network");
+	lua_setglobal(L, "Config");
 
 	if (luaL_dofile(L, path.c_str()) != LUA_OK) {
 		std::cerr << "ERROR: Error while loading Lua config: " << lua_tostring(L, -1) << '\n';
 		return;
 	}
 
-	lua_getglobal(L, "Content");
+	lua_getglobal(L, "Config");
 	if (lua_isnil(L, -1)) {
-		std::cerr << "ERROR: Content is nil\n";
+		std::cerr << "ERROR: 'Config' is nil\n";
+		lua_settop(L, 0);
+		return;
 	}
-	else {
-		lua_getfield(L, -1, "textures");
-
-		if (!lua_isnil(L, -1)) {
-			if (lua_istable(L, -1)) {
-				lua_pushnil(L);
-				while (lua_next(L, -2)) {
-					if (lua_isstring(L, -1)) {
-						textures.push_back(lua_tostring(L, -1));
-					}
-					lua_pop(L, 1);
+	
+	lua_getfield(L, -1, "textures");
+	if (!lua_isnil(L, -1)) {
+		if (lua_istable(L, -1)) {
+			lua_pushnil(L);
+			while (lua_next(L, -2)) {
+				if (lua_isstring(L, -1)) {
+					textures.push_back(lua_tostring(L, -1));
 				}
+				lua_pop(L, 1);
+			}
+		}
+		else {
+			std::cerr << "ERROR: Config.textures is not an array\n";
+		}
+	}
+
+	lua_pop(L, 1);
+	lua_getfield(L, -1, "title");
+	if (!lua_isnil(L, -1)) {
+		if (lua_isstring(L, -1)) {
+			windowTitle = lua_tostring(L, -1);
+		}
+		else {
+			std::cerr << "ERROR: Config.title is not a string\n";
+		}
+	}
+
+	lua_pop(L, 1);
+	lua_getfield(L, -1, "width");
+	if (!lua_isnil(L, -1)) {
+		if (lua_isinteger(L, -1)) {
+			lua_Integer i = lua_tointeger(L, -1);
+			if (i > 0) {
+				windowWidth = static_cast<std::uint32_t>(i);
 			}
 			else {
-				std::cerr << "ERROR: Content.textures is not an array\n";
+				std::cerr << "ERROR: Config.width must be greater than 0\n";
 			}
+		}
+		else {
+			std::cerr << "ERROR: Config.width is not an integer\n";
+		}
+	}
+
+	lua_pop(L, 1);
+	lua_getfield(L, -1, "height");
+	if (!lua_isnil(L, -1)) {
+		if (lua_isinteger(L, -1)) {
+			lua_Integer i = lua_tointeger(L, -1);
+			if (i > 0) {
+				windowHeight = static_cast<std::uint32_t>(i);
+			}
+			else {
+				std::cerr << "ERROR: Config.height must be greater than 0\n";
+			}
+		}
+		else {
+			std::cerr << "ERROR: Config.height is not an integer\n";
+		}
+	}
+
+	lua_pop(L, 1);
+	lua_getfield(L, -1, "port");
+	if (!lua_isnil(L, -1)) {
+		if (lua_isinteger(L, -1)) {
+			lua_Integer i = lua_tointeger(L, -1);
+			if (i > 0 && i < UINT16_MAX) {
+				port = static_cast<std::uint16_t>(i);
+			}
+			else {
+				std::cerr << "ERROR: Config.port must be between 1 and 65535\n";
+			}
+		}
+		else {
+			std::cerr << "ERROR: Config.port is not an integer\n";
+		}
+	}
+
+	lua_pop(L, 1);
+	lua_getfield(L, -1, "max_players");
+
+	if (!lua_isnil(L, -1)) {
+		if (lua_isinteger(L, -1)) {
+			lua_Integer i = lua_tointeger(L, -1);
+			if (i > 0) {
+				maxPlayers = static_cast<std::uint32_t>(i);
+			}
+			else {
+				std::cerr << "ERROR: Config.max_players must be greater than 0\n";
+			}
+		}
+		else {
+			std::cerr << "ERROR: Config.max_players is not an integer\n";
 		}
 	}
 
 	lua_settop(L, 0);
-
-	lua_getglobal(L, "Window");
-	if (lua_isnil(L, -1)) {
-		std::cerr << "ERROR: Window is nil\n";
-	}
-	else {
-		lua_getfield(L, -1, "title");
-
-		if (!lua_isnil(L, -1)) {
-			if (lua_isstring(L, -1)) {
-				windowTitle = lua_tostring(L, -1);
-			}
-			else {
-				std::cerr << "ERROR: Window.title is not a string\n";
-			}
-		}
-
-		lua_pop(L, 1);
-		lua_getfield(L, -1, "width");
-
-		if (!lua_isnil(L, -1)) {
-			if (lua_isinteger(L, -1)) {
-				lua_Integer i = lua_tointeger(L, -1);
-				if (i > 0) {
-					windowWidth = static_cast<std::uint32_t>(i);
-				}
-				else {
-					std::cerr << "ERROR: Window.width must be greater than 0\n";
-				}
-			}
-			else {
-				std::cerr << "ERROR: Window.width is not an integer\n";
-			}
-		}
-
-		lua_pop(L, 1);
-		lua_getfield(L, -1, "height");
-
-		if (!lua_isnil(L, -1)) {
-			if (lua_isinteger(L, -1)) {
-				lua_Integer i = lua_tointeger(L, -1);
-				if (i > 0) {
-					windowHeight = static_cast<std::uint32_t>(i);
-				}
-				else {
-					std::cerr << "ERROR: Window.height must be greater than 0\n";
-				}
-			}
-			else {
-				std::cerr << "ERROR: Window.height is not an integer\n";
-			}
-		}
-	}
-
-	lua_settop(L, 0);
-
-	lua_getglobal(L, "Network");
-	if (lua_isnil(L, -1)) {
-		std::cerr << "ERROR: Network is nil\n";
-	}
-	else {
-		lua_getfield(L, -1, "port");
-
-		if (!lua_isnil(L, -1)) {
-			if (lua_isinteger(L, -1)) {
-				lua_Integer i = lua_tointeger(L, -1);
-				if (i > 0 && i < UINT16_MAX) {
-					port = static_cast<std::uint16_t>(i);
-				}
-				else {
-					std::cerr << "ERROR: Port must be between 1 and 65535\n";
-				}
-			}
-			else {
-				std::cerr << "ERROR: Network.port is not an integer\n";
-			}
-		}
-
-		lua_pop(L, 1);
-		lua_getfield(L, -1, "max_players");
-		
-		if (!lua_isnil(L, -1)) {
-			if (lua_isinteger(L, -1)) {
-				lua_Integer i = lua_tointeger(L, -1);
-				if (i > 0) {
-					maxPlayers = static_cast<std::uint32_t>(i);
-				}
-				else {
-					std::cerr << "ERROR: Network.max_players must be greater than 0\n";
-				}
-			}
-			else {
-				std::cerr << "ERROR: Network.max_players is not an integer\n";
-			}
-		}
-	}
 }
 
 const std::vector<std::string>& Config::GetTextures() const {
